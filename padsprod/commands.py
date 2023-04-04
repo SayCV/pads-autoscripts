9c933105441823b7357146911a4ab42e4c3281a8
SayCV <sssxyc@163.com>
2023-04-05 07:37:07 +0800

    add catch exception to support avoid running many application instance in background

--- padsprod/commands.py
+++ padsprod/commands.py
@@ -7,6 +7,7 @@ import sys
 from pathlib import Path as path
 
 from . import hyp_util, pcb_util, sch_util
+from .exceptions import PadsprodException
 
 logger = logging.getLogger(__name__)
 
@@ -26,20 +27,29 @@ def run_info(args):
     if in_format == 'sch':
         board_file = input
         set_visible = False
-        sch = sch_util.SCH(args, board_file, set_visible)
-        sch.info()
+        try:
+            sch = sch_util.SCH(args, board_file, set_visible)
+            sch.info()
+        except PadsprodException as e:
+            logger.error(e)
         sch.close()
     elif in_format == 'pcb':
         board_file = input
         set_visible = False
-        pcb = pcb_util.PCB(args, board_file, set_visible)
-        pcb.info()
+        try:
+            pcb = pcb_util.PCB(args, board_file, set_visible)
+            pcb.info()
+        except PadsprodException as e:
+            logger.error(e)
         pcb.close()
     elif in_format == 'hyp' or in_format == 'pjh':
         board_file = input
         set_visible = False
-        hyp =hyp_util.HYP(args, board_file, set_visible)
-        hyp.save_components_nets()
+        try:
+            hyp =hyp_util.HYP(args, board_file, set_visible)
+            hyp.save_components_nets()
+        except PadsprodException as e:
+            logger.error(e)
         hyp.close()
     else:
         logger.info(f"Info Command Unimplemented! -- {in_format}")
@@ -75,48 +85,54 @@ def run_export(args):
         if out_format == 'pdf':
             sch_file = input
             visible = False
-            if (args.start_page is not None and args.end_page is not None and args.start_page > args.end_page):
-                logger.error(f"Provided start page({args.start_page}) over than the end page({args.end_page})!")
-                sch.close()
-                return
+            try:
+                if (args.start_page is not None and args.end_page is not None and args.start_page > args.end_page):
+                    logger.error(f"Provided start page({args.start_page}) over than the end page({args.end_page})!")
+                    sch.close()
+                    return
 
-            sch = sch_util.SCH(args, sch_file, visible)
-            if args.start_page is not None and args.start_page > sch.get_total_sheet_count():
-                logger.warning(f"Provided start page({args.start_page}) over than the sch real sheet count({sch.get_total_sheet_count()})!")
-                args.start_page = sch.get_total_sheet_count()
-                #sch.close()
-                #return
-            elif args.end_page is not None and args.end_page > sch.get_total_sheet_count():
-                logger.warning(f"Provided end page({args.end_page}) over than the sch real sheet count({sch.get_total_sheet_count()})!")
-                args.end_page = sch.get_total_sheet_count()
-                #sch.close()
-                #return
-            elif args.page > sch.get_total_sheet_count():
-                logger.error(f"Provided sheet number({args.page}) over than the sch real sheet count({sch.get_total_sheet_count()})!")
-                sch.close()
-                return
-            start_page = args.start_page
-            end_page = args.end_page
-            if args.start_page:
-                if args.end_page is None:
-                    end_page = sch.get_total_sheet_count()
-            if args.end_page:
-                if args.start_page is None:
-                    start_page = 1
+                sch = sch_util.SCH(args, sch_file, visible)
+                if args.start_page is not None and args.start_page > sch.get_total_sheet_count():
+                    logger.warning(f"Provided start page({args.start_page}) over than the sch real sheet count({sch.get_total_sheet_count()})!")
+                    args.start_page = sch.get_total_sheet_count()
+                    #sch.close()
+                    #return
+                elif args.end_page is not None and args.end_page > sch.get_total_sheet_count():
+                    logger.warning(f"Provided end page({args.end_page}) over than the sch real sheet count({sch.get_total_sheet_count()})!")
+                    args.end_page = sch.get_total_sheet_count()
+                    #sch.close()
+                    #return
+                elif args.page > sch.get_total_sheet_count():
+                    logger.error(f"Provided sheet number({args.page}) over than the sch real sheet count({sch.get_total_sheet_count()})!")
+                    sch.close()
+                    return
+                start_page = args.start_page
+                end_page = args.end_page
+                if args.start_page:
+                    if args.end_page is None:
+                        end_page = sch.get_total_sheet_count()
+                if args.end_page:
+                    if args.start_page is None:
+                        start_page = 1
 
-            sch.run_macro_plog_reset_default_palette()
+                sch.run_macro_plog_reset_default_palette()
 
-            if start_page and end_page:
-                for page in range(start_page, end_page + 1):
-                    sch.run_macro_plog_export_pdf(output, page)
-            else:
-                sch.run_macro_plog_export_pdf(output, args.page)
+                if start_page and end_page:
+                    for page in range(start_page, end_page + 1):
+                        sch.run_macro_plog_export_pdf(output, page)
+                else:
+                    sch.run_macro_plog_export_pdf(output, args.page)
+            except PadsprodException as e:
+                logger.error(e)
             sch.close()
         elif out_format == 'txt':
             sch_file = input
             visible = False
-            sch = sch_util.SCH(args, sch_file, visible)
-            sch.export_ascii(output)
+            try:
+                sch = sch_util.SCH(args, sch_file, visible)
+                sch.export_ascii(output)
+            except PadsprodException as e:
+                logger.error(e)
             sch.close()
         else:
             logger.error("Output format not support")
@@ -126,35 +142,43 @@ def run_export(args):
         if out_format == 'pdf':
             board_file = input
             visible = False
-            pcb = pcb_util.PCB(args, board_file, visible)
-            if args.layer > pcb.get_total_layer_count():
-                logger.error(f"Provided layer number({args.layer}) over than the pcb real layer count({pcb.get_total_layer_count()})!")
-                pcb.close()
-                return
+            try:
+                pcb = pcb_util.PCB(args, board_file, visible)
+                if args.layer > pcb.get_total_layer_count():
+                    logger.error(f"Provided layer number({args.layer}) over than the pcb real layer count({pcb.get_total_layer_count()})!")
+                    pcb.close()
+                    return
 
-            pcb.run_macro_ppcb_reset_default_palette()
-            #pcb.run_macro_ppcb_export_pdf(output, 'Top')
-            #pcb.run_macro_ppcb_export_pdf(output, 'Bottom')
-            if args.layer == 0:
-                for idx in range(1, pcb.get_electrical_layer_count() + 1):
-                    pcb.run_macro_ppcb_export_pdf(output, idx)
-                drawing_layer_id = pcb.get_layer_id('Drill Drawing')
-                pcb.run_macro_ppcb_export_pdf(output, drawing_layer_id)
-            else:
-                pcb.run_macro_ppcb_export_pdf(output, args.layer)
-
+                pcb.run_macro_ppcb_reset_default_palette()
+                #pcb.run_macro_ppcb_export_pdf(output, 'Top')
+                #pcb.run_macro_ppcb_export_pdf(output, 'Bottom')
+                if args.layer == 0:
+                    for idx in range(1, pcb.get_electrical_layer_count() + 1):
+                        pcb.run_macro_ppcb_export_pdf(output, idx)
+                    drawing_layer_id = pcb.get_layer_id('Drill Drawing')
+                    pcb.run_macro_ppcb_export_pdf(output, drawing_layer_id)
+                else:
+                    pcb.run_macro_ppcb_export_pdf(output, args.layer)
+            except PadsprodException as e:
+                logger.error(e)
             pcb.close()
         elif out_format == 'asc':
             board_file = input
             visible = False
-            pcb = pcb_util.PCB(args, board_file, visible)
-            pcb.export_ascii(output)
+            try:
+                pcb = pcb_util.PCB(args, board_file, visible)
+                pcb.export_ascii(output)
+            except PadsprodException as e:
+                logger.error(e)
             pcb.close()
         elif out_format == 'hyp':
             board_file = input
             visible = False
-            pcb = pcb_util.PCB(args, board_file, visible)
-            pcb.export_hyp(output)
+            try:
+                pcb = pcb_util.PCB(args, board_file, visible)
+                pcb.export_hyp(output)
+            except PadsprodException as e:
+                logger.error(e)
             pcb.close()
         else:
             logger.error("Output format not support")
@@ -189,11 +213,14 @@ def run_renamerefs(args):
 
         sch_file = input
         visible = False
-        sch = sch_util.SCH(args, sch_file, visible)
-        sch.run_renamerefs()
-        if output is None:
-            output = input.with_suffix('.refs-renamed.sch')
-        sch.save_as(path(output).absolute())
+        try:
+            sch = sch_util.SCH(args, sch_file, visible)
+            sch.run_renamerefs()
+            if output is None:
+                output = input.with_suffix('.refs-renamed.sch')
+            sch.save_as(path(output).absolute())
+        except PadsprodException as e:
+            logger.error(e)
         sch.close(False)
         logger.status(f"Renamerefs {out_format} done.")
     elif in_format == 'pcb':
@@ -217,8 +244,11 @@ def run_simu(args):
     if in_format == 'hyp' or in_format == 'pjh':
         board_file = input
         set_visible = False
-        hyp = hyp_util.HYP(args, board_file, set_visible)
-        hyp.save_components_nets()
+        try:
+            hyp = hyp_util.HYP(args, board_file, set_visible)
+            hyp.save_components_nets()
+        except PadsprodException as e:
+            logger.error(e)
         hyp.close()
     else:
         logger.info(f"Simu Command Unimplemented! -- {in_format}")
